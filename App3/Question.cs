cf1652c6a4f29dc56b14af7d384015bbca906297
Jakub Swierczek <jakub.swierczek@outlook.com>
2015-12-12 12:17:45 +0100

    Questions ;)

---
+++ App3/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+        public Answer RightAnswer { get; set; }
+
+        public enum Answer
+        {
+            A,
+            B,
+            C,
+            D
+        };
+    }
+}
---
+++ App3/QuestionContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace App3
+{
+    public class QuestionContext
+    {
+        public DbSet<Question> Questions { get; set; }
+
+    }
+}
