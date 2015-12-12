0a3e2d8681449706a9f6d4ae7e30128fe63291c2
Marcin Gurbiel <gurbielmarcin@gmail.com>
2015-12-12 13:15:48 +0100

    s
    das

---
+++ App3/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string LogicalGate { get; set; }
+        public string RightAnswer { get; set; }
+
+    }
+}
---
+++ App3/QuestionContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace App3
+{
+    public class QuestionContext
+    {
+        private string fileName;
+        public List<Question> Questions { get; set; }
+
+        private String[] arrayStrings = new[]
+        {"and", "0001", "nand", "1110", "nor", "1000", "not", "10", "or", "0111", "xor", "0110"};
+
+        public QuestionContext()
+        {
+            Questions = new List<Question>();
+            fileName = "Quizies.txt";
+            GetQuestionsFromFile();
+            
+        }
+
+        private void GetQuestionsFromFile()
+        {
+            var spiltedText = arrayStrings.ToList();
+            for (var i = 0; i < spiltedText.Count; i += 2)
+            {
+                Questions.Add(new Question()
+                {
+                    LogicalGate = spiltedText[i],
+                    RightAnswer = spiltedText[i + 1]
+                });
+            }
+
+        }
+    }
+}
